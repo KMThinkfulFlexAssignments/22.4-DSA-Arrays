e0ea3f2f386a084172d5e15640bbea1c207053b6
Kevin Moore <moore.kevin.p@gmail.com>
2020-05-22 10:18:50 -0400

    finished through question 11

--- drills.js
+++ drills.js
@@ -73,7 +73,7 @@ function filterArry(arry, filter) {
 //let testArry = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 //console.log(filterArry(testArry, 5));
 
-//6. Max sum in the array
+//7. Max sum in the array
 function maxSum(arry) {
   let result = 0;
   for(let i = 0; i < arry.length; i++) {
@@ -83,3 +83,67 @@ function maxSum(arry) {
 }
 //let testArry = [4, 6, -3, 5, -2, 1];
 //console.log(maxSum(testArry));
+
+//8. Merge arrays
+function mergeArrays(arry1, arry2){
+  let result = arry1.concat(arry2);
+  return result.sort(function(a, b){return a-b;});
+}
+//let testArry1 = [1, 3, 6, 8, 11];
+//let testArry2 = [2, 3, 5, 8, 9, 10];
+//console.log(mergeArrays(testArry1, testArry2));
+
+//9. Remove characters
+function removeCharacters(str, omit) {
+  let result = '';
+  for(let i = 0; i < str.length; i++) {
+    if(!omit.includes(str[i])) result += str[i];
+  }
+  return result;
+}
+//let testStr = 'Here are some words';
+//let testOmit = 'aeiou';
+//console.log(removeCharacters(testStr, testOmit));
+
+//10. Products
+function multiplyArray(arry) {
+  let prod = arry.reduce((prod, num) => prod *= num, 1);
+  const result = arry.map(num => prod / num);
+  return result;
+}
+//let testArry = [1, 3, 9, 4];
+//console.log(multiplyArray(testArry));
+
+//11. 2D array
+function twoDArry(arry) {
+  let row = [];
+  let column = [];
+  for(let i = 0; i < arry.length; i ++) {
+    if(arry[i].includes(0)) row.push(i);
+  }
+  for(let num in row) {
+    for(let j = 0; j < arry[num].length; j++) {
+      if(arry[num][j] === 0 && !column.includes(0)) {
+        column.push(j);
+      }
+    }
+  }
+  for (let y = 0; y < arry.length; y++) {
+    for (let x = 0; x < arry[y].length; x++) {
+      if (column.includes(x) || row.includes(y)) {
+        arry[y][x] = 0;
+      }
+    }
+  }
+  return arry;
+}
+//let testArry = [
+//  [1,0,1,1,0],
+//  [0,1,1,1,0],
+//  [1,1,1,1,1],
+//  [1,0,1,1,1],
+//  [1,1,1,1,1]
+//];
+//console.log(twoDArry(testArry));
+
+//12. Strong rotation
